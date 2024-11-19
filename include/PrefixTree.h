c1223b70bc41c711ab83c63d29ff9d2a86ded8f1
Artem Romanov <ArtemRomanov2017@yandex.ru>
2024-11-19 17:11:22 +0300

    added PrefixTree

--- include/PrefixTree.h
+++ include/PrefixTree.h
@@ -1,3 +1,72 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+template<typename PrefixTreeType> struct PrefixTree {
+	struct Node {
+		std::map<char, int> next;
+		PrefixTreeType* data = nullptr;
+		bool is_terminal = false;
+
+		~Node() {
+			delete data;
+			data = nullptr;
+		}
+	};
+
+	std::vector<Node> nodes;
+
+	PrefixTree() {
+		nodes.push_back(Node());
+	}
+
+	bool nextStateExist(int curr_state, char C) {
+		return (nodes[curr_state].next.find(C) != nodes[curr_state].next.end());
+	}
+
+	void createNextState(int curr_state, char C) {
+		nodes[curr_state].next[C] = nodes.size();
+		nodes.push_back(Node());
+	}
+
+	int getNextState(int curr_state, char C) {
+		return nodes[curr_state].next[C];
+	}
+
+	void addWord(const std::string& S, PrefixTreeType* data_ptr) {
+		int curr_state = 0;
+		for (char C : S) {
+			if (!nextStateExist(curr_state, C)) {
+				createNextState(curr_state, C);
+			}
+			curr_state = getNextState(curr_state, C);
+		}
+		nodes[curr_state].data = data_ptr;
+		nodes[curr_state].is_terminal = true;
+	}
+
+	bool exists(const std::string& S) {
+		int curr_state = 0;
+		for (char C : S) {
+			if (!nextStateExist(curr_state, C)) {
+				return false;
+			}
+			curr_state = getNextState(curr_state, C);
+		}
+		return nodes[curr_state].is_terminal;
+	}
+
+	PrefixTreeType* getData(const std::string& S) {
+		int curr_state = 0;
+		for (char C : S) {
+			if (!nextStateExist(curr_state, C)) {
+				throw std::out_of_range("The requested word is not in the prefix tree!");
+			}
+			curr_state = getNextState(curr_state, C);
+		}
+		return nodes[curr_state].data;
+	}
+};
--- test/test_PrefixTree.cpp
+++ test/test_PrefixTree.cpp
@@ -1,9 +1,59 @@
 #include <gtest.h>
 
+#include <string>
+
 #include "PrefixTree.h"
 
-TEST(NameOfTheTestedClass, just_an_empty_test) {
+TEST(PrefixTree, can_create) {
 
-	EXPECT_EQ(1, 1);
+	ASSERT_NO_THROW(PrefixTree<void> tree);
 
+}
+
+TEST(PrefixTree, can_add_words) {
+
+	PrefixTree<int> tree;
+
+	ASSERT_NO_THROW(tree.addWord("abcd", nullptr));
+	ASSERT_NO_THROW(tree.addWord("abce", nullptr));
+	ASSERT_NO_THROW(tree.addWord("f", nullptr));
+}
+
+TEST(PrefixTree, can_check_words) {
+
+	PrefixTree<int> tree;
+
+	tree.addWord("abcd", nullptr);
+	tree.addWord("abce", nullptr);
+	tree.addWord("f", nullptr);
+
+	EXPECT_EQ(tree.exists("abcd"), true);
+	EXPECT_EQ(tree.exists("abce"), true);
+	EXPECT_EQ(tree.exists("f"), true);
+
+	EXPECT_EQ(tree.exists("a"), false);
+	EXPECT_EQ(tree.exists("bfs"), false);
+}
+
+TEST(PrefixTree, can_get_data) {
+
+	PrefixTree<int> tree;
+
+	int* a = new int;
+	int* b = new int;
+
+	*a = 5;
+	*b = 3;
+
+	tree.addWord("abcd", a);
+	tree.addWord("f", b);
+
+	EXPECT_EQ(*tree.getData("abcd"), 5);
+	EXPECT_EQ(*tree.getData("f"), 3);
+
+	ASSERT_ANY_THROW(tree.getData("123"));
+	EXPECT_EQ(tree.exists("f"), true);
+
+	EXPECT_EQ(tree.exists("a"), false);
+	EXPECT_EQ(tree.exists("bfs"), false);
 }
