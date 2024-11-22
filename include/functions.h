7a26b6b5b93a20f2f4382b36d9a1ea9c968e8c20
Artem Romanov <106965054+Quaha@users.noreply.github.com>
2024-11-22 10:54:38 +0300

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -2,12 +2,4 @@
 
 #include "includes.h"
 
-Data __RETURN__IT__SELF__( // ()
-	const std::vector<IntegerData>& integer_parameters,
-	const std::vector<RealData>& real_parameters,
-	const std::vector<type>& types_order);
-
-Data max( // max(value1, value2, ... )
-	const std::vector<IntegerData>& integer_parameters,
-	const std::vector<RealData>& real_parameters,
-	const std::vector<type>& types_order);
+Data __RETURN__IT__SELF__(const std::vector<Data>& parameters); // ()
