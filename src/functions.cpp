c685bfeeae6f9ffc95daa7b2db6453a9d0fefd17
Artem Romanov <106965054+Quaha@users.noreply.github.com>
2024-11-22 13:34:48 +0300

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -7,19 +7,20 @@ Data __RETURN__IT__SELF__(const std::vector<Data>& parameters) { // ()
 	}
 	
 	Data result;
-
+	result.type = parametres[0].type;
+	
 	switch (parameters[0].type) {
 		case INTEGER:
-
+			
 		        break;
 		case REAL:
-
+			
 		        break;
 		case VARIABLE:
-
+			
 			break;
 		default:
-
+			throw std::invalid_argument("Function __RETURN__IT__SELF__ error!");
 			break;
 	}
 
