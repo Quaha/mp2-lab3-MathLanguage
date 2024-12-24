0602e685187e11ba9717b80f403be1e014cb8fe9
Quaha <ArtemRomanov2017@yandex.ru>
2024-12-24 10:11:29 +0300

    Other fixes

--- test/test_PrefixTree.cpp
+++ test/test_PrefixTree.cpp
@@ -6,8 +6,8 @@
 TEST(PrefixTree, addAndGetData) {
     PrefixTree<string, string, char> tree;
 
-    tree.addWord("apple", "A fruit");
-    tree.addWord("banana", "Another fruit");
+    tree.setWord("apple", "A fruit");
+    tree.setWord("banana", "Another fruit");
 
     EXPECT_EQ(tree.getData("apple"), "A fruit");
     EXPECT_EQ(tree.getData("banana"), "Another fruit");
@@ -16,7 +16,7 @@ TEST(PrefixTree, addAndGetData) {
 TEST(PrefixTree, wordExists) {
     PrefixTree<string, string, char> tree;
 
-    tree.addWord("apple", "A fruit");
+    tree.setWord("apple", "A fruit");
 
     EXPECT_TRUE(tree.exists("apple"));
     EXPECT_FALSE(tree.exists("banana"));
@@ -25,23 +25,23 @@ TEST(PrefixTree, wordExists) {
 TEST(PrefixTree, nonExistentWordThrowsException) {
     PrefixTree<string, string, char> tree;
 
-    tree.addWord("apple", "A fruit");
+    tree.setWord("apple", "A fruit");
 
     ASSERT_ANY_THROW(tree.getData("banana"));
 }
 
 TEST(PrefixTree, emptyWord) {
     PrefixTree<string, string, char> tree;
-    tree.addWord("", "An empty word");
+    tree.setWord("", "An empty word");
 
     EXPECT_EQ(tree.getData(""), "An empty word");
 }
 
-TEST(PrefixTree, addWordsAndCheckOrder) {
+TEST(PrefixTree, setWordsAndCheckOrder) {
     PrefixTree<int, string, char> tree;
 
-    tree.addWord("apple", 1);
-    tree.addWord("banana", 2);
+    tree.setWord("apple", 1);
+    tree.setWord("banana", 2);
 
     EXPECT_EQ(tree.getData("apple"), 1);
     EXPECT_EQ(tree.getData("banana"), 2);
@@ -49,15 +49,15 @@ TEST(PrefixTree, addWordsAndCheckOrder) {
 
 TEST(PrefixTree, LongChainOfWords) {
     PrefixTree<string, string, char> tree;
-    tree.addWord("thisisaverylongword", "A very long word");
+    tree.setWord("thisisaverylongword", "A very long word");
 
     EXPECT_EQ(tree.getData("thisisaverylongword"), "A very long word");
 }
 
 TEST(PrefixTree, handleMultipleWordsWithCommonPrefix) {
     PrefixTree<string, string, char> tree;
-    tree.addWord("apple", "A fruit");
-    tree.addWord("apricot", "Another fruit");
+    tree.setWord("apple", "A fruit");
+    tree.setWord("apricot", "Another fruit");
 
     EXPECT_EQ(tree.getData("apple"), "A fruit");
     EXPECT_EQ(tree.getData("apricot"), "Another fruit");
@@ -65,8 +65,8 @@ TEST(PrefixTree, handleMultipleWordsWithCommonPrefix) {
 
 TEST(PrefixTree, handleNestedWords) {
     PrefixTree<string, string, char> tree;
-    tree.addWord("hello", "A greeting");
-    tree.addWord("hell", "A word meaning underworld");
+    tree.setWord("hello", "A greeting");
+    tree.setWord("hell", "A word meaning underworld");
 
     EXPECT_EQ(tree.getData("hello"), "A greeting");
     EXPECT_EQ(tree.getData("hell"), "A word meaning underworld");
